b363e7389ccbe3e85d830a23a2d96307ce70882d
9321451 <158720950+9321451@users.noreply.github.com>
2025-07-28 09:33:05 +0500

    hjkhkhjh

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -10,4 +10,22 @@ import { RouterModule } from '@angular/router';
 })
 export class EducationComponent {
    educationList = education;
+
+   ngAfterViewInit(): void {
+  const cards = document.querySelectorAll('.animate-on-scroll');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const animationClass = entry.target.getAttribute('data-animation');
+        entry.target.classList.add('animate__animated', animationClass || 'animate__fadeInUp');
+        observer.unobserve(entry.target); // animate once
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  cards.forEach(card => observer.observe(card));
+}
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,22 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-    ngAfterViewInit(): void {
-  const cards = document.querySelectorAll('.animate-on-scroll');
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const animationClass = entry.target.getAttribute('data-animation');
-        entry.target.classList.add('animate__animated', animationClass || 'animate__fadeInUp');
-        observer.unobserve(entry.target); // animate once
-      }
-    });
-  }, {
-    threshold: 0.1
-  });
-
-  cards.forEach(card => observer.observe(card));
-}
+    
 
 }
